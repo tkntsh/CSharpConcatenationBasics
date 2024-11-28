4c132e37b260c13247197db776abb5f2735caa7a
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:10:46 +0200

    Create concatenation9.cs
    Can you tidy up the code from the previous task so your nickname is called as a variable? (Remember: it still needs the “nickname” in quotation marks).

---
+++ concatenation9.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Ntokozo";
+            string sName = "Ntshangase";
+            string nickname = "The Artist";
+            Console.WriteLine("From this day forward, " + fName + "" + sName + " shall be known as:\n" + fName + " \"" + nickname + "\" " + sName);
+        }
+    }
+}
