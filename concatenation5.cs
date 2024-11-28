7c26a3ee8d0bef685b2e6c8dfd583ef521fe7607
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:03:55 +0200

    Create concatenation5.cs
    Create two variables named car owned and car want.
    Assign values to both variables.
    Produce the following:
    
    "I own a VW, I would like a Skoda"

---
+++ concatenation5.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string carOwned = "VW";
+            string carWant = "Skoda";
+            Console.WriteLine("I own a " + carOwned + ", I would like a " + carWant);
+        }
+    }
+}
