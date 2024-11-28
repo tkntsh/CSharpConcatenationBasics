eb6002fc7e30b793d72299636517ac150445e0e6
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:02:20 +0200

    Create concatenation3.cs
    Add the text Today’s date is into the Console so it produces something like the following:
    
    "Todays date is 12th December 2019"

---
+++ concatenation3.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2024;
+            string month = "April";
+            int date = 04;
+            Console.WriteLine("Today's date is " + date + "th " + month + " " + year);
+        }
+    }
+}
