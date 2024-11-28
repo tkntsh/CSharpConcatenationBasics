b8a454ffeb7f0dd2877fbfb8caccaf73c543f572
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:00:38 +0200

    Create concatenation2.cs
    Create a variable for home team.
    Assign a value to your variable.
    Create a second variable for away team.
    Assign a value to your second variable.
    Concatenate the variables so they display home team vs away team in the Console.

---
+++ concatenation2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string homeTeam = "Real Madrid";
+            string awayTeam = "Man City";
+            Console.WriteLine(homeTeam + " vs " + awayTeam);
+        }
+    }
+}
