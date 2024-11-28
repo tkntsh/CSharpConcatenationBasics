fea4619c159c366b209dcb9b53dd4cfc7bbecc9e
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:00:03 +0200

    Create concatenation1.cs
    Create a variable to hold your first name.
    Assign a value to your variable.
    Create a second variable to hold your surname.
    Assign a value to your second variable.
    Concatenate the variables so they display first name and surname separated by a gap.
    Call your variable in the Console.

---
+++ concatenation1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Ntokozo";
+            string sName = "Ntshangase";
+            Console.WriteLine(fName + " " + sName);
+        }
+    }
+}
