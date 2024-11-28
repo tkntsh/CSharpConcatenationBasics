c0b52bc70256355c2f547c2ea66a04057a11534b
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:09:48 +0200

    Create concatenation8.cs
    Create a variable to hold your first name and assign a value.
    Create a second variable to hold your surname and assign a value.
    Concatenate the variables so they display first name and surname separated by a gap.
    Give yourself a nickname and display something like the following:
    
    "From this day forward, Jake Roberts shall known as: Jack "The Snake" Roberts"

---
+++ concatenation8.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Ntokozo";
+            string sName = "Ntshangase";
+            Console.WriteLine("From this day forward, " + fName + "" + sName + " shall be known as:\n" + fName + " \"The Artist\" " + sName);
+        }
+    }
+}
